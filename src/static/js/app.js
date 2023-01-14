7ac76e76128ee8ffa266b1894022ab0025d97f9c
dodo3095 <84676304+dodo3095@users.noreply.github.com>
2023-01-15 08:18:22 +0900

    Fix header

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,10 +2,10 @@ function App() {
     const { Container, Row, Col, Navbar, Nav } = ReactBootstrap;
     return (
         <Container>
-            <Nav className="mr-auto">
-                <Nav.Link href="#toDO App by dodo3095">Home</Nav.Link>
-                </Nav>
-            </Navbar >
+//             <Nav className="mr-auto">
+//                 <Nav.Link href="#toDO App by dodo3095">Home</Nav.Link>
+//                 </Nav>
+//             </Navbar >
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
@@ -55,6 +55,7 @@ function TodoListCard() {
 
     return (
         <React.Fragment>
+            <h1> ToDo App by Aki Ando </h1>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
